937386b8f01bc73f9d0e7afbe31f8e10c72d1e9c
erik <erik@twitter.com>
2012-10-10 09:27:22 -0700

    remove marionette from files

--- public/js/main.js
+++ public/js/main.js
@@ -4,16 +4,11 @@ require.config({
     'zepto': 'libs/jquery.min',
     "underscore": "libs/underscore-min",
     "backbone": "libs/backbone-min",
-    "backbone-marionette": "libs/backbone-marionette",
     "bootstrap": "libs/bootstrap.min"
   },
   'shim': {
     backbone: {
       'deps': ['zepto', 'underscore'],
-      'exports': 'Backbone'
-    },
-    'backbone-marionette': {
-      'deps': ['backbone'],
       'exports': 'Backbone'
     },
     underscore: {
