81218fb43f05fce4afd887709ac3873b4e8722a2
erik <erik@twitter.com>
2012-10-20 22:59:09 -0700

    integrate suggestion & draft views into typeahead

--- public/js/views/typeahead_input.js
+++ public/js/views/typeahead_input.js
@@ -7,12 +7,48 @@ define([
   'text!templates/typeahead_input.html'
 ], function module(_, Backbone,
   itemCollection, userCollection,
-  typeaheadSuggestionsTemplate, template){
+  typeaheadSuggestionsTemplate, typeaheadInputTemplate){
 
-  var suggestionCollection = new Backbone.Collection();
+  var suggestionCollection = new Backbone.Collection(),
+      phraseCollection = new Backbone.Collection();
 
-  var SuggestionView = Backbone.View.extend({
+  var DraftView = Backbone.View.extend({
+        initialize: function(){
+          phraseCollection.on('add remove', this.render, this);
+          this.render();
+        },
+        remove: function(){
+          this.undelegateEvents();
+          Backbone.View.prototype.remove.call(this);
+        },
+        render: function(){
+          var text = phraseCollection.pluck('text').join(' ');
+          this.$el.html( text );
+          return this;
+        }
+      });
+
+  var SuggestionsView = Backbone.View.extend({
         template: _.template(typeaheadSuggestionsTemplate),
+        events: {
+          'click .suggestion': 'select'
+        },
+        initialize: function(){
+          suggestionCollection.on('add reset', this.render, this);
+          this.render();
+        },
+        remove: function(){
+          this.undelegateEvents();
+          Backbone.View.prototype.remove.call(this);
+        },
+        select: function(e){
+          this.trigger('select');
+          var $target = $(e.target),
+              modelId = $target.data('model-id'),
+              suggestion = suggestionCollection.where({model_id: modelId})[0];
+          phraseCollection.push(suggestion.toJSON());
+          suggestionCollection.reset();
+        },
         render: function(){
           var html = this.template({
             suggestions: suggestionCollection.toArray()
@@ -22,10 +58,10 @@ define([
         }
       });
 
-  var View = Backbone.View.extend({
-    template: _.template( template ),
+  var TypeaheadInputView = Backbone.View.extend({
+    template: _.template( typeaheadInputTemplate ),
     events: {
-      'keyup input': 'dispatch'
+      'keyup input': 'handleKey'
     },
     initialize: function(){
       this.render();
@@ -35,71 +71,74 @@ define([
       Backbone.View.prototype.remove.call(this);
     },
     render: function(){
-      var html = this.template();
+      var html = this.template(),
+          suggestionsView = new SuggestionsView(),
+          draftView = new DraftView();
       this.$el.html( html );
-      var suggestionView = new SuggestionView();
-      suggestionCollection.on('add reset', function(){
-        this.$('.suggestions').html(suggestionView.render().el);
+      this.$input = this.$('input');
+      this.on('remove', suggestionsView.remove);
+      this.on('remove', draftView.remove);
+      this.$('.suggestions').html(suggestionsView.render().el);
+      this.$('.draft').html(draftView.render().el);
+      suggestionsView.on('select', function(){
+        this.$input.val('').focus();
       }, this);
-    },
-    dispatch: function(e){
-      var text = e.target.value.replace(/^\s+|\s+$/g, '');
-      if(!text && 8 === e.keyCode){// if input is empty, and we hit backspace, remove last phrase
-        suggestionCollection.reset();
-      }else if(text && 32 === e.keyCode){// on space, add word to phrase collection
-      }else if(13 === e.keyCode){ // return
-        this.compile();
-      }else if(text){
-        this.suggest(text);
-      }
     },
     suggest: function(text){
       var name,
           re = new RegExp(text);
-          itemCollection.each(function(model){
-            name = model.get('name');
-            if(re.test(name)){
-              suggestionCollection.add({
-                type: 'item',
-                id: 'item-'+model.get('id'),
-                text: name
-              });
-            }
+      suggestionCollection.reset();
+      itemCollection.each(function(model){
+        name = model.get('name');
+        if(re.test(name)){
+          suggestionCollection.add({
+            model_id: 'item-'+model.get('id'),
+            text: name
           });
-          userCollection.each(function(model){
-            name = model.get('name');
-            if(re.test(name)){
-              suggestionCollection.add({
-                type: 'user',
-                id: 'user-'+model.get('id'),
-                text: name
-              });
-            }
+        }
+      });
+      userCollection.each(function(model){
+        name = model.get('name');
+        if(re.test(name)){
+          suggestionCollection.add({
+            model_id: 'user-'+model.get('id'),
+            text: name
           });
+        }
+      });
     },
-    compile: function(){
-      var $input = this.$('input'),
-          text = $input.val();
-      if(text){
-        this.phrases.push(text);
+    handleKey: function(e){
+      var text = this.$input.val().replace(/^\s+|\s+$/g, ''),
+          $draft = this.$('.draft');
+      switch(e.keyCode){
+        case 8: // backspace
+          if(!text){
+            phraseCollection.pop();
+            suggestionCollection.reset();
+          }
+          break;
+        case 13: // return
+          if(text){
+            phraseCollection.push({text: text});
+          }
+          this.$input.val('');
+          this.trigger('return', {text: $draft.text()});
+          $draft.empty();
+          break;
+        case 32: // space
+          if(text){
+            phraseCollection.push({text: text});
+          }
+          this.$input.val('');
+          break;
+        default:
+          if(text){
+            this.suggest(text);
+          }
       }
-      $input.val('');
-      text = this.phrases.join(' '),
-      this.trigger('return', {text: text});
-      this.phrases = [];
-      this.$('.phrases').empty();
-    },
-    select: function(e){
-      var $target = $(e.target),
-          text = $target.text().replace(/^\s+|\s+$/g, '');
-      this.phrases.push(text);
-      if('item' === $target.data('model')){
-        this.selectedSuggestions.push(itemCollection.get($target.data('id')));
-      }
-      this.render();
-      this.$('input').focus();
-    },
+    }
   });
-  return View;
+
+  return TypeaheadInputView;
 
 });
