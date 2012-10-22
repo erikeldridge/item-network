9229987a6911cd3f36aa41fd13362b4e18962b5e
erik <erik@twitter.com>
2012-10-21 21:13:14 -0700

    populate comment template on comment detail page

--- public/js/views/show_comment_page.js
+++ public/js/views/show_comment_page.js
@@ -2,9 +2,10 @@ define([
   'underscore',
   'backbone',
   'collections/comments',
-  'views/comment_tag',
+  'collections/items',
+  'collections/users',
   'text!templates/show_comment_page.html'
-], function module(_, Backbone, commentCollection, CommentTagView, template){
+], function module(_, Backbone, commentCollection, itemCollection, userCollection, template){
 
   var View = Backbone.View.extend({
     template: _.template( template ),
@@ -18,10 +19,25 @@ define([
       Backbone.View.prototype.remove.call(this);
     },
     render: function(){
+      var text = this.comment.get('text'),
+          itemIds = text.match(/{item-(\d+)}/) || [],
+          userIds = text.match(/{user-(\d+)}/) || [];
+      text = text.replace(/{([^-]+-\d+)}/g, '<a data-model-id="$1"></a>');
+      itemIds.shift();
+      userIds.shift();
       var html = this.template({
-            comment: this.comment
+            comment: this.comment,
+            text: text
           });
       this.$el.html( html );
+      _.each(itemIds, function(id){
+        var item = itemCollection.get(id);
+        this.$('a[data-model-id=item-'+id+']').attr('href', '/items/'+id).html(item.get('name'));
+      }, this);
+      _.each(userIds, function(id){
+        var user = userCollection.get(id);
+        this.$('a[data-model-id=user-'+id+']').attr('href', '/users/'+id).html(user.get('name'));
+      }, this);
     }
   });
   return View;
